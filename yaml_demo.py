6eefe46de6cb50788614ca4805ffd64b421b026f
LiuJinbao <liu.jinbao@outlook.com>
2019-09-08 23:56:05 +0800

    2019.09.08

--- yaml_demo.py
+++ yaml_demo.py
@@ -131,9 +131,12 @@ class Cros_Reaction_block(Reaction_block):
     def _set_threshold_list(self):
         self._threshold = self.rctn_dict['threshold']
         _iter = self.rctn_dict['iterator']
-        self._threshold_list = eval(self.repl_func(self._threshold,
-                                                   _iter['repl']['threshold'],
-                                                   _iter))
+        if 'threshold' in _iter['repl']:
+            self._threshold_list = eval(self.repl_func(self._threshold,
+                                                       _iter['repl']['threshold'],
+                                                       _iter))
+        else:
+            self._threshold_list = self._threshold
 
     def generate_crostn_dataframe(self, *, factor=1):
         _df = dict()
