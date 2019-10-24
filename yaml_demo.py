27cd73f5e9bdefd1d8550d58b0f0a5ea317d0d96
Unknown <liu.jinbao@outlook.com>
2019-10-24 16:39:34 +0800

    2019.10.24

--- yaml_demo.py
+++ yaml_demo.py
@@ -20,7 +20,8 @@ from plasmistry.io import (standard_Arr_constructor,
                            Coef_Reaction_block,
                            Cros_Reaction_block)
 from plasmistry.molecule import (CO2_vib_energy_in_K, CO2_vib_energy_in_eV,
-                                 H2_vib_energy_in_K, H2_vib_energy_in_eV)
+                                 H2_vib_energy_in_K, H2_vib_energy_in_eV,
+                                 CO_vib_energy_in_K, CO_vib_energy_in_eV)
 
 yaml.add_constructor("!StandardArr", standard_Arr_constructor)
 yaml.add_constructor("!ChemKinArr_2_rcnt", chemkin_Arr_2_rcnts_constructor)
@@ -33,19 +34,64 @@ yaml.add_constructor("!F_gamma", F_gamma_constructor)
 vari_dict = {'CO2_vib_energy_in_K': CO2_vib_energy_in_K,
              'H2_vib_energy_in_K': H2_vib_energy_in_K,
              'H2_vib_energy_in_eV': H2_vib_energy_in_eV,
-             'CO2_vib_energy_in_eV': CO2_vib_energy_in_eV}
+             'CO2_vib_energy_in_eV': CO2_vib_energy_in_eV,
+             'CO_vib_energy_in_eV': CO_vib_energy_in_eV,
+             'CO_vib_energy_in_K': CO_vib_energy_in_K}
 # ---------------------------------------------------------------------------- #
 if __name__ == "__main__":
     with open("./_yaml/test_0.yaml") as f:
         rctn = yaml.load(f)
-    rctn_rel = rctn[-1]['The reactions considered']['relaxation reactions']
-    rctn_ele = rctn[-1]['The reactions considered']['electron reactions']
+    rctn_test = rctn[-1]['Test']['test']
+    # rctn_ele = rctn[-1]['The reactions considered']['electron reactions']
     global_abbr = rctn[-1]['The reactions considered']['global_abbr']
-    rctn_block = Coef_Reaction_block(rctn_dict=rctn_rel[
-        'H2_VT_with_CO2_forward'],
+    rctn_block = Coef_Reaction_block(rctn_dict=rctn_test,
                                      vari_dict=vari_dict,
                                      global_abbr=global_abbr)
-    rctn_block_ele = Cros_Reaction_block(rctn_dict=rctn_ele[
-        'H2_ele_vib_forward'],
-                                         vari_dict=vari_dict,
-                                         global_abbr=global_abbr)
+    a = rctn_block.generate_crostn_dataframe()
+    # rctn_block_ele = Cros_Reaction_block(rctn_dict=rctn_ele[
+    #     'H2_ele_vib_forward'],
+    #                                      vari_dict=vari_dict,
+    #                                      global_abbr=global_abbr)
+
+
+    for i in range(22):
+        for j in range(12):
+            energy = CO2_vib_energy_in_eV(v=(0,0,i)) + \
+                CO_vib_energy_in_eV(v=j) - \
+                CO2_vib_energy_in_eV(v=(0,0,i-1)) - \
+                CO_vib_energy_in_eV(v=j+1)
+            print(f"CO2 {i}->{i-1}   CO {j}->{j+1} energy {energy}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
